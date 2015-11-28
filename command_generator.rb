cd65a39251b0fc0492996de5b861b60e59c84d17
spectre6000 <spectre6000@gmail.com>
2015-11-28 15:46:48 -0700

    interpreter compelete, needs server

--- command_generator.rb
+++ command_generator.rb
@@ -8,78 +8,94 @@
 require 'socket'
 require 'json'
 
-# Flow control
-def parse_to_astrobot( file )
-  raw_json_string( file )
-  pull_commands
-  parse_string
-  eval_string
-  json_formatter
-end
+class CommandGenerator
 
-# Temp holder values
-@object = ""
-@raw_command_string = ""
-@parsed_command_string = []
-@output = ""
-
-# Access JSON object input
-def raw_json_string( file )
-  @object = JSON.parse( open( file ).read )
-end
-
-# Pull pertinent data from JSON input
-def pull_commands
-  string = ""
-  @object[ 'token_values' ].each do | token |
-    string << "#{ token[ 'token' ] } " if token[ 'token' ] != 'blank'
+  def initialize( file )
+    @object = JSON.parse( open( file ).read )
+    @structures = [  /((while|for|if) ... [0-9])/,
+                    /(((x =)|(y =)) [0-9])|(((x =)|(y =)) ... [0-9])/,
+                    /((f|b|l|r) (10|20|30|40|50|60|70|80|90))/,
+                    /(end)/ ]
+    parse_to_astrobot
   end
-  @raw_command_string = string[ 0 .. -2 ]
-end
 
-# Break command string into lines
-def parse_string
-  holder = []
-  structures = [  /((while|for|if) ... [0-9])/,
-                  /(((x =)|(y =)) [0-9])|(((x =)|(y =)) ... [0-9])/,
-                  /((f|b|l|r) (10|20|30|40|50|60|70|80|90))/,
-                  /(end)/ ]
-  structures.each do | structure |
-    if structure.match( @raw_command_string )
-      @raw_command_string.scan( structure ).each do | caught |
-        found = caught.to_a.compact.max_by( &:length )
-        start = /#{ Regexp.quote( found ) }/.match( @raw_command_string ).begin( 0 )
-        substitute = []
-        found.length.times { substitute << "~" }
-        @raw_command_string.sub!( structure,  substitute.join )
-        holder << [ start, found ]
+  # Flow control
+  def parse_to_astrobot
+    pull_commands
+    parse_string
+    eval_string
+    json_formatter
+  end
+
+  # Pull pertinent data from JSON input
+  def pull_commands
+    @raw_command_string = ""
+    @object[ 'token_values' ].each do | token |
+      @raw_command_string << "#{ token[ 'token' ] } " if  token[ 'token' ] != nil
+    end
+  end
+
+  # Break command string into lines
+  def parse_string
+    holder = []
+    @structures.each do | structure |
+      if structure.match( @raw_command_string )
+        @raw_command_string.scan( structure ).each do | caught |
+          found = caught.to_a.compact.max_by( &:length )
+          start = /#{ Regexp.quote( found ) }/.match( @raw_command_string ).begin( 0 )
+          substitute = []
+          found.length.times { substitute << "~" }
+          @raw_command_string.sub!( structure,  substitute.join )
+          holder << [ start, found ]
+        end
       end
     end
+    @parsed_command_string = holder.sort
   end
-  @parsed_command_string = holder.sort
-end
 
-# Reassemble and eval command string for output commands
-def eval_string
-  holder = []
-  output = []
-  @parsed_command_string.each do | subcommand |
-    if subcommand[ 1 ].match( /((f|b|l|r) (10|20|30|40|50|60|70|80|90))/ )
-      subcommand[ 1 ] = "output.push( '#{ subcommand[ 1 ] }' )"
+  # Reassemble and eval command string for output commands
+  def eval_string
+    holder = []
+    output = []
+    @parsed_command_string.each do | subcommand |
+      if subcommand[ 1 ].match( /((f|b|l|r) (10|20|30|40|50|60|70|80|90))/ )
+        subcommand[ 1 ] = "output.push( '#{ subcommand[ 1 ] }' )"
+      end
+      holder << subcommand[ 1 ]
     end
-    holder << subcommand[ 1 ]
+    executable = holder.join( "; " )
+    eval( executable )
+    @command_array = output
   end
-  executable = holder.join( "; " )
-  eval( executable )
-  print output
+
+  # Format into output JSON object
+  def json_formatter
+    json_output = { "robot_commands" => @command_array.length, "command_values" => [] }
+    command_body = @command_array.each do | command |
+      json_output[ "command_values" ] << { "line" => "#{ @command_array.index( command ) }", "value" => "#{ command }" } 
+      @command_array[ @command_array.index( command ) ] = ""
+    end 
+    @output = json_output.to_json
+  end
+
+  # Output to robot
+  def send_to_astro_bot
+
+  end
+
 end
 
-# Format into output JSON object
-def json_formatter
+# class AstroBotServer
 
-end
+#   @server = TCPServer.open( 9999 )      # Socket to listen on port 9999
 
-# Output to robot
-def send_to_astro_bot
+#   loop {                                # Servers run forever
+#     Thread.start( @server.accept ) do | client |
+#       file = socket.read                # Read complete response
+#       @command_generator = CommandGenerator.new( file )
+#       client.print( @command_generator.instance_variable_get( :@output ) )
+#       client.close                      # Disconnect from the client
+#     end
+#   }
 
-end
+# end
--- command_generator_spec.rb
+++ command_generator_spec.rb
@@ -2,7 +2,6 @@ require './command_generator'
 require 'socket'
 require 'json'
 
-# @singlecmd = JSON.parse( open(File.dirname(__FILE__) + '/json_samples/1_command/3-translator_output-1cmd.json').read )
 single_cmd_input = File.dirname(__FILE__) + '/json_samples/1_command/3-translator_output-1cmd.json'
 single_cmd_output = File.dirname(__FILE__) + '/json_samples/1_command/5-robot_input-1cmd.json'
 square_cmd_input = File.dirname(__FILE__) + '/json_samples/square_command/3-translator_output-square.json'
@@ -10,23 +9,23 @@ square_cmd_output  = File.dirname(__FILE__) + '/json_samples/square_command/5-ro
 while_cmd_input = File.dirname(__FILE__) + '/json_samples/while_command/3-translator_output-while.json'
 while_cmd_output = File.dirname(__FILE__) + '/json_samples/while_command/5-robot_input-while.json'
 
-describe 'command parser' do 
+describe 'CommandGenerator' do 
+
+  let( :command_generator_single ) { CommandGenerator.new( single_cmd_input ) }
 
   it 'translates single command scenario input into expected Astrobot JSON object' do
-    parse_to_astrobot( single_cmd_input )
-    expect( @output ).to eq( JSON.parse( open( single_cmd_output ).read ) )
+    command_generator = CommandGenerator.new( single_cmd_input )
+    expect( JSON.parse( command_generator.instance_variable_get( :@output ) ) ).to eq( JSON.parse( open( single_cmd_output ).read ) )
   end
 
   it 'translates square command scenario input into expected Astrobot JSON object' do
-    parse_to_astrobot( square_cmd_input )
-    expect( @output ).to eq( JSON.parse( open( square_cmd_output ).read ) )
+    command_generator = CommandGenerator.new( square_cmd_input )
+    expect( JSON.parse( command_generator.instance_variable_get( :@output ) ) ).to eq( JSON.parse( open( square_cmd_output ).read ) )
   end
 
   it 'translates while command scenario input into expected Astrobot JSON object' do
-    parse_to_astrobot( while_cmd_input )
-    expect( @output ).to eq( JSON.parse( open( while_cmd_output ).read ) )
-  end
-
-  
+    command_generator = CommandGenerator.new( while_cmd_input )
+    expect( JSON.parse( command_generator.instance_variable_get( :@output ) ) ).to eq( JSON.parse( open( while_cmd_output ).read ) )
+  end  
 
 end
